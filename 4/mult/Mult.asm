ac59adc364932f8b13cc266bf9b45d19bbe78dbe
Andreas Goss <andreas.goss1994@gmail.com>
2025-02-17 21:13:58 +0100

    Multiplication Assembly code

---
+++ 4/mult/Mult.asm
@@ -0,0 +1,33 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/4/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// The algorithm is based on repetitive addition.
+
+@i
+M=1
+@R2
+M=0
+(LOOP)
+@i
+D=M
+@R0
+D=D-M
+@END
+D;JGT
+@R2
+D=M
+@R1
+D=D+M
+@R2
+M=D
+@i 
+M=M+1
+@LOOP
+0;JMP
+(END)
+@END
+0;JMP
