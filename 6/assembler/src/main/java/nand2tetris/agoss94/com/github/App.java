700b346f099b0aed85496c3733d40d1f1fd223b4
Andreas Goss <andreas.goss1994@gmail.com>
2025-02-21 09:44:11 +0100

    Setup assembler project

---
+++ 6/assembler/src/main/java/nand2tetris/agoss94/com/github/App.java
@@ -0,0 +1,10 @@
+package nand2tetris.agoss94.com.github;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ 6/assembler/src/test/java/nand2tetris/agoss94/com/github/AppTest.java
@@ -0,0 +1,19 @@
+package nand2tetris.agoss94.com.github;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
